a35c1f995a2627b0f2bb904a5796266ede92376c
Davison <davisonvinicius@outlook.com.br>
2026-05-27 21:03:17 -0300

    fix:validates, enum from dimension and added verification to active in check_base_exists

--- app/models/measurement_unit.rb
+++ app/models/measurement_unit.rb
@@ -7,20 +7,21 @@ class MeasurementUnit < ApplicationRecord
   before_validation :check_base_exists, on: [ :create, :update ]
 
   validates :code, presence: true, uniqueness: true
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: true
   validates :dimension, presence: true
+  validates :is_base, inclusion: { in: [ true, false ] }
   validates :active, inclusion: { in: [ true, false ] }
   validates :created_by, presence: true
   validates :conversion_factor, presence: true, numericality: { greater_than: 0 }
 
-  enum dimension: { mass: 0, volume: 1, unit: 2 }
+  enum :dimension, { mass: 0, volume: 1, unit: 2 }
 
   belongs_to :created_by, class_name: "User"
 
   private
 
   def check_base_exists
-    return unless is_base?
+    return unless is_base? && active?
 
     has_base = MeasurementUnit.where(dimension: dimension, is_base: true, active: true)
       .where.not(id: id)
