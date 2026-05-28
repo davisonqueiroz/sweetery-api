2cdf36d2078b7bcd6c8bb8019906d2c209edb382
Davison <davisonvinicius@outlook.com.br>
2026-05-27 21:03:38 -0300

    fix: added indexes to code and name

--- db/migrate/20260524190103_create_measurement_units.rb
+++ db/migrate/20260524190103_create_measurement_units.rb
@@ -1,17 +1,16 @@
 class CreateMeasurementUnits < ActiveRecord::Migration[8.1]
   def change
     create_table :measurement_units do |t|
-      t.string :code, null: false
-      t.string :name, null: false
+      t.string :code, null: false, index: true
+      t.string :name, null: false, index: true
       t.integer :dimension, null: false
-      t.boolean :is_base, default: false
+      t.boolean :is_base, null: false, default: false
       t.decimal :conversion_factor, null: false,  precision: 10, scale: 2
       t.boolean :active, null: false, default: true
       t.references :created_by, null: false, foreign_key: { to_table: :users }, index: false
 
       t.timestamps
 
-      t.index :code, unique: true
       t.index :dimension, unique: true, where: "is_base = true AND active = true"
       t.check_constraint "conversion_factor > 0", name: "conversion_check"
     end
